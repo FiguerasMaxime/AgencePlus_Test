373a1cc5703122d8495243c4d46acf6d970235a3
FiguerasMaxime <maxime.figueras@gmail.com>
2020-05-05 18:23:54 +0200

    :fire:Responsive correction and fix burger menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 const arrow = document.getElementById("arrow");
 const form = document.getElementById("form");
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("nav-links");
+
 
 arrow.addEventListener("click", function(){
     arrow.classList.toggle("toggle");
     form.classList.toggle("clicked")
+})
+
+burger.addEventListener("click", function(){
+    navLinks.classList.toggle("open");
+    burger.classList.toggle("close");
 })
